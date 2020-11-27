5f63955c018ccbe169c87ff5c9b23f06cedd07e3
qn10163 <1150887961@qq.com>
2020-11-27 10:19:22 +0800

    推送spingmvc项目至github

---
+++ src/main/java/com/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "demo")
+public class DemoController {
+
+    @RequestMapping(path = "index", method = RequestMethod.GET)
+    @ResponseBody
+    public String index() {
+        return "Hello springmvc.";
+    }
+
+}
